f6067017e8154f7fa6ed1e37c4c1dcb537346d5e
PaolaArz <paolarzuffi@gmail.com>
2019-09-11 21:04:15 +0100

    Updates

---
+++ precache-manifest.e4e51ca09aee00c97c55a9d357848736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184f817ad47a5564bed085802ec630e8",
+    "url": "/kittens-rolodex/index.html"
+  },
+  {
+    "revision": "6e38afd25fb5ef517ec5",
+    "url": "/kittens-rolodex/static/css/main.7f9bf0ec.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/kittens-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "6e38afd25fb5ef517ec5",
+    "url": "/kittens-rolodex/static/js/main.41e06438.chunk.js"
+  },
+  {
+    "revision": "c2eb9d17358df7ed7e2b",
+    "url": "/kittens-rolodex/static/js/runtime~main.a2f7d5b2.js"
+  }
+]);
